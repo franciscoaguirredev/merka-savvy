02cfd6951cdda35563df2e31bfc51197a65e15d4
franciscoaguirredev <163943262+franciscoaguirredev@users.noreply.github.com>
2024-09-08 15:12:07 -0500

    new folder src added again

--- src/modules/base-list/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './base-list.module'
-export * from './base-list.controller'
-export * from './base-list.service'
--- src/modules/customer/interfaces/customer.service.interface.ts
+++
@@ -1,7 +0,0 @@
-import { CreateCustomerDto } from "../dto/create-customer.dto";
-import { Customer } from "../entities/customer.entity";
-
-
-export interface ICustomerService{
-    // create(createCustomerDto: CreateCustomerDto): Promise<Customer>;
-}
