64c29ba8d5f1f69e39adb54257d14118a852dac6
Andres Hernandez <pipehernandez1998@gmail.com>
2024-09-10 20:39:33 -0500

    customer CRUD done

--- src/modules/customer/customer.controller.ts
+++ src/modules/customer/customer.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Post, Body, Inject } from '@nestjs/common';
+import { Controller, Post, Body, Patch, Param, NotFoundException, Delete, HttpCode, Get } from '@nestjs/common';
 import { CreateCustomerDto } from './dto/create-customer.dto';
 import { Customer } from './entities/customer.entity';
 import { CustomerService } from './customer.service';
+import { UpdateCustomerDto } from './dto/update-customer.dto';
 
 @Controller('customers')
 export class CustomerController {
@@ -13,7 +14,38 @@ export class CustomerController {
   async create(@Body() createCustomerDto: CreateCustomerDto): Promise<Customer> {
     // console.log(createCustomerDto);
     return await this.customerService.create(createCustomerDto);
-    
+  }
+  
+  @Patch('update/:email')
+  async update(@Param('email') email: string, @Body() updateCustomerDto: UpdateCustomerDto): Promise<Customer>{
+    const updatedCustomer = await this.customerService.update(email, updateCustomerDto);
+    if(!updatedCustomer){
+      throw new NotFoundException(`Customer with email ${email} not found`);
+    }
+    return updatedCustomer;
   }
 
+  @Delete('delete/:email')
+  @HttpCode(200)
+  async remove(@Param('email') email: string): Promise<{ message: string }> {
+    const result = await this.customerService.remove(email);
+    if(!result){
+      throw new NotFoundException(`Customer with email ${email} not found`);
+    }
+    return { message: `Customer with email ${email} deleted succesfully` };
+  }
+
+  @Get()
+  async findAll(): Promise<Customer[]> {
+    return await this.customerService.getAllCustomers();
+  }
+
+  @Get('/:email')
+  async findOne(@Param('email') email: string): Promise<Customer> {
+    const customer = await this.customerService.getByEmail(email);
+    if(!customer){
+      throw new NotFoundException(`Customer with email ${email} doesn't exist`);
+    }
+    return customer;
+  }
 }
--- src/modules/customer/dto/update-customer.dto.ts
+++ src/modules/customer/dto/update-customer.dto.ts
@@ -1,4 +1,17 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateCustomerDto } from './create-customer.dto';
+import { IsOptional, IsString } from 'class-validator';
 
-export class UpdateCustomerDto extends PartialType(CreateCustomerDto) {}
+export class UpdateCustomerDto extends PartialType(CreateCustomerDto) {
+    @IsOptional()
+    @IsString()
+    readonly name?: string;
+
+    @IsOptional()
+    @IsString()
+    readonly email?: string;
+
+    @IsOptional()
+    @IsString()
+    readonly password?: string;
+}
