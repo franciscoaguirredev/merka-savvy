185d3ad3df7ba1729899f2949bd177e6d91d07a6
Andres Hernandez <pipehernandez1998@gmail.com>
2024-09-11 17:35:53 -0500

    comments eliminated

--- src/modules/customer/entities/customer.entity.ts
+++ src/modules/customer/entities/customer.entity.ts
@@ -1,3 +1,4 @@
+
 import { BaseList } from "src/modules/base-list/entities/base-list.entity";
 import { PurchaseList } from "src/modules/purchase-list/entities/purchase-list.entity";
 import { Column, CreateDateColumn, Entity, OneToMany, OneToOne, PrimaryGeneratedColumn, Timestamp, UpdateDateColumn } from "typeorm";
@@ -25,24 +26,9 @@ export class Customer {
         select: false, 
         nullable: false })
     password: string;
-    
-    // @CreateDateColumn(
-        // {
-        // name: 'created_at',
-        // type: 'timestamptz',
-        // default: () => `CURRENT_TIMESTAMP AT TIME ZONE 'GMT-5'`}
-    // )
 
     @CreateDateColumn({ type: 'timestamp' })
     registrationDate: Date;
-
-    // @UpdateDateColumn(
-    //     {
-    // name: 'updated_at',
-    // type: 'timestamptz',
-    // default: () => `CURRENT_TIMESTAMP AT TIME ZONE 'GMT-5'`,
-    // onUpdate: `CURRENT_TIMESTAMP AT TIME ZONE 'GMT-5'`,}
-    // )
 
     @UpdateDateColumn({ type: 'timestamp' })
     lastUpdateDate: Date;
