830cf9ce438d321699d4e0dabec69eb13f2cd389
franciscoaguirredev <163943262+franciscoaguirredev@users.noreply.github.com>
2024-09-11 20:21:24 -0500

    Seed Roles created (#16)
    Checked by Franco

---
+++ src/modules/database/roles.seed.ts
@@ -0,0 +1,29 @@
+import { Seeder} from 'typeorm-extension';
+import { DataSource } from 'typeorm';
+import { Role } from '../roles';
+
+
+export default class CreateRoles implements Seeder{
+    public async run(dataSource: DataSource): Promise<void> {
+        const roleRepository = dataSource.getRepository(Role)
+
+        const rolesData = [
+            {
+                name:'administrador'
+            },
+            {
+                name: 'cliente'
+            }
+        ];
+
+        for (const role of rolesData){
+            const roleExist = await roleRepository.findOneBy({name:role.name})
+            if(!roleExist){
+                const newRole = roleRepository.create(role)
+                await roleRepository.save(newRole)
+            }
+        } 
+        console.log('Seed Roles loaded');
+    }
+    
+}
