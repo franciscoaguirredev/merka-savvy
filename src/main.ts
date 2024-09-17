cda1852670dc0ac9e7082ce74f57b58bbd56de46
Andres Hernandez <pipehernandez1998@gmail.com>
2024-09-16 19:54:03 -0500

    guardJWT done

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { Logger as logger } from '@nestjs/common';
+import { ValidationPipe, Logger as logger } from '@nestjs/common';
 
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe(
+    {
+      whitelist: true,
+      transform: true,
+      forbidNonWhitelisted: true,
+    },
+  ))
   await app.listen(3000);
   logger.log(`App running on port 3000`);
 }
