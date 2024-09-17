a345877954b0da821d7f0dba20b7572ee6a47e74
franciscoaguirredev <163943262+franciscoaguirredev@users.noreply.github.com>
2024-09-17 18:29:50 -0500

    Eliminación de todas las carpetas para evitar conflictos en el proximo merge

--- src/modules/base-list/dto/update-base-list.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateBaseListDto } from './create-base-list.dto';
-
-export class UpdateBaseListDto extends PartialType(CreateBaseListDto) {}
--- src/modules/common/config/env.config.ts
+++
@@ -1,7 +0,0 @@
-export const EnvConfig = () => ({
-  host: process.env.DB_HOST,
-  port: parseInt(process.env.DB_PORT) || 3306,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  name: process.env.DB_DATABASE,
-});
